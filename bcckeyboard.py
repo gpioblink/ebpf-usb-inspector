6e94a48193f1341a30108f8c7d04b5d90549978f
Kai Yamada <gpioblink@aol.com>
2020-09-10 15:39:45 +0900

    show key value

--- bcckeyboard.py
+++ bcckeyboard.py
@@ -1,6 +1,7 @@
 from bcc import BPF
 from bcc.utils import printb
 from hexdump import hexdump
+import re
 
 code = """
 #include <linux/usb.h>
@@ -34,14 +35,28 @@ b = BPF(text=code)
 def print_event(cpu, data, size):
     event = b["events"].event(data)
     if event.vendor == 0x05ac: # change this id to your USB device
-      print("[vendor = 0x%x, product = 0x%x] transfer_flags = %s, actual_length = %d, transfer_buffer_length = %d" % (event.vendor, event.product, judge_in_out(event.transfer_flags), event.alen, event.buflen))
-      hexdump(bytes(event.buf[0:event.buflen]))
-      print("")
+      keycodes = []
+      for c in bytes(event.buf[0:event.buflen]):
+        keycodes.append(asciichar(c))
+      print("%s %s (%s)" % (judge_in_out(event.transfer_flags), keycodes, re.sub(r"\s+", " ", hexdump(bytes(event.buf[0:event.buflen]), result="return"))))
 
 def judge_in_out(transfer_flags):
   if transfer_flags & 0x0200 != 0:
-    return "IN"
-  return "OUT"
+    return "IN >"
+  return "OUT<"
+
+def asciichar(keynum):
+    char = chr(keynum)
+    if 3 < ord(char) < 40:
+        char = chr(keynum + 61)
+
+    # TODO: ここ見ながらキーコード追加！
+    altchars = {
+        chr(0): '',
+        chr(40): 'ENTER',
+        chr(43): 'TAB'
+    }
+    return altchars.get(char, char)
 
 b["events"].open_perf_buffer(print_event)
 while 1:
